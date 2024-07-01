1ca470a9b159352fe492e7af2b13fad45dd7b177
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-07-01 17:40:38 +0200

    downscale tile selector

--- scripts/ui/cell_indicator.gd
+++ scripts/ui/cell_indicator.gd
@@ -1,11 +1,13 @@
 class_name CellIndicator
-extends Sprite2D
+extends Node2D
+
+@onready var panel: Panel = %Panel
 
 func green():
-	modulate = Color.LIME_GREEN
+	panel.modulate = Color.LIME_GREEN
 
 func red():
-	modulate = Color.ORANGE_RED
+	panel.modulate = Color.ORANGE_RED
 
 func place_at_cell(tile_map: TileMap, cell_coords: Vector2i):
 	var coords = tile_map.map_to_local(cell_coords)
